a6ebe427bb9407766a20f56cc9be44675e83a65e
stark <serj.elessar@gmail.com>
2023-10-16 14:26:30 +0300

    какой то результат

--- src/components/ChatBody.tsx
+++ src/components/ChatBody.tsx
@@ -4,17 +4,34 @@ import Message from './Message';
 interface IMessage {
 	sender: 'bot' | 'you',
 	message: string;
+	id: string;
 }
 
 interface IProps {
-	allMessages: IMessage[] | null;
+	allMessages: IMessage[];
 	result: string;
+	isTyping: boolean;
 }
 
-const ChatBody: FC<IProps> = ({ allMessages, result }) => {
+const ChatBody: FC<IProps> = ({ allMessages = [], result, isTyping  }) => {
 	return (
 		<div className="m-chat-body">
-			<Message isMe={false} message={result}/>
+			{
+				allMessages.map(({ sender, message, id }) => {
+					return <Message isMe={sender === 'you'} message={message} key={id}/>
+				})
+			}
+
+			{
+				isTyping && (
+					<div className="loader">
+						печатает.. &nbsp;
+						<span></span>
+						<span></span>
+						<span></span>
+					</div>
+				)
+			}
 		</div>
 	);
 };
