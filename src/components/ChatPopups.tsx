d0b9757aa2c71ce860ca94e7ce79fefef7f90026
stark <serj.elessar@gmail.com>
2024-01-23 00:49:00 +0300

    feat: add popup components

---
+++ src/components/ChatPopups.tsx
@@ -0,0 +1,83 @@
+import { Fragment, MouseEvent, useState } from 'react';
+
+import { useActions, useAppDispatch, useAppSelector } from '../hooks/redux-hooks';
+import { ChatAPI } from '../api/chat-api';
+import CustomRating from './CustomRating';
+
+const ChatPopups = () => {
+	const isVisibleFeedbackPopup = useAppSelector(({ feedback }) => feedback.openedPopup);
+	return (
+		<Fragment>
+			{ isVisibleFeedbackPopup && <FeedbackPopup/> }
+		</Fragment>
+	);
+};
+
+const FeedbackPopup = () => {
+	const dispatch = useAppDispatch();
+	const [rating, setRating] = useState(0);
+	const [feedbackText, setFeedbackText] = useState('');
+	const { isLoadingFeedback, messageId } = useAppSelector(({ feedback }) => feedback);
+	const {
+		togglePopup,
+		setMessageId,
+		changeFeedbackStatus
+	} = useActions();
+
+	const onClosePopup = ({ target, currentTarget }: MouseEvent<HTMLDivElement>) => {
+		if (target !== currentTarget) return;
+		togglePopup(false);
+	};
+
+	const sendFeedback = async () => {
+		if (!messageId) return;
+
+		try {
+			const response = dispatch(ChatAPI.sendFeedback({ id: messageId, text_response: feedbackText, rating }));
+			await response.unwrap();
+			changeFeedbackStatus({ id: messageId });
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setMessageId(null);
+			setFeedbackText('');
+			setRating(0);
+			togglePopup(false);
+		}
+	};
+
+	return (
+		<div className="m-chat-popup" onClick={onClosePopup}>
+			<div className="m-chat-popup__body">
+				<div className="m-chat-popup__title">Оставить отзыв</div>
+				<textarea
+					placeholder="Напишите короткий отзыв об ответе.."
+					value={feedbackText}
+					onChange={({ target }) => setFeedbackText(target.value)}
+				></textarea>
+
+				<div className="m-chat-popup__rating">
+					Дайте оценку ответу:
+					<CustomRating rating={rating} setRating={setRating}/>
+				</div>
+
+				<button
+					onClick={sendFeedback}
+					className="m-chat-popup__button"
+				>
+					Отправить
+				</button>
+
+				{
+					!isLoadingFeedback && (
+						<div className="m-chat-popup__loader">
+							<span className="loader"></span>
+						</div>
+					)
+				}
+			</div>
+		</div>
+	)
+};
+
+export default ChatPopups;
