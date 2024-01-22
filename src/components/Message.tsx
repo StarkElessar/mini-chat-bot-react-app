3c073ca4706958ac07862a89280888fd9310ecf0
stark <serj.elessar@gmail.com>
2024-01-23 00:49:54 +0300

    fix: update message component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,34 +1,68 @@
-import { FC, memo, ReactElement } from 'react';
+import { FC, Fragment, memo, ReactElement } from 'react';
 import ReactMarkdown, { Components } from 'react-markdown';
 
 import { IMessage } from '../types';
-import BotIcon from '../assets/images/cookie-icon.svg';
-import UserIcon from '../assets/images/user-icon.png';
+import { MessageAvatar } from './MessageAvatar';
+import { FeedbackIcon } from './icons';
+import { useActions } from '../hooks/redux-hooks';
 
 interface IProps {
 	isMe: boolean;
 	message: IMessage;
 }
 
-const Message: FC<IProps> = memo(({ isMe, message: { message, type_message} }): ReactElement => {
+const Message: FC<IProps> = memo((props): ReactElement => {
+	const {
+		isMe,
+		message: {
+			message,
+			type_message,
+			id,
+			has_feedback
+		}
+	} = props;
+	const { togglePopup, setMessageId } = useActions();
+
 	const componentsOptions: Components = {
 		a: ({ node, ...props}) => <a target="_blank" {...props}/>
 	};
 
+	const onFeedbackClickHandle = () => {
+		console.log({id});
+		togglePopup(true);
+		setMessageId(id);
+	};
+
 	return (
-		<div className={ `m-chat-message ${isMe ? 'user' : ''}` }>
-			<div className="m-chat-message__image">
-				<img src={isMe ? UserIcon : BotIcon} alt="Avatar"/>
-			</div>
+		<div className={ `m-chat-message ${isMe ? 'user' : 'bot'}` }>
+			<MessageAvatar isMe={isMe}/>
 
 			{
 				type_message === 'message'
 					? (
+						<Fragment>
+							<div className="m-chat-message__text">
+								<ReactMarkdown components={componentsOptions}>{message}</ReactMarkdown>
+							</div>
+							{
+								!isMe && !has_feedback && (
+									<div className="m-chat-message__options">
+										<button
+											title="Дать оценку ответу"
+											onClick={onFeedbackClickHandle}
+											className="m-chat-message__button"
+										>
+											<FeedbackIcon/>
+										</button>
+									</div>
+								)
+							}
+						</Fragment>
+					) : (
 						<div className="m-chat-message__text">
-							<ReactMarkdown components={componentsOptions}>{message}</ReactMarkdown>
+							<img src={ message } alt="Photo"/>
 						</div>
 					)
-					: <div className="m-chat-message__text"><img src={message} alt="Photo"/></div>
 			}
 		</div>
 	);
