340999b33c3205c6e2c8f785cdc6ed72587fb519
stark <serj.elessar@gmail.com>
2024-01-23 00:46:56 +0300

    feat: add custom rating component

---
+++ src/components/CustomRating.tsx
@@ -0,0 +1,22 @@
+import '@smastrom/react-rating/style.css'
+
+import { FC } from 'react';
+import { Rating, RatingChange, Star } from '@smastrom/react-rating';
+
+interface IProps {
+	rating: number;
+	setRating: RatingChange;
+}
+
+const CustomRating: FC<IProps> = ({ rating, setRating }) => {
+	return (
+		<Rating
+			value={rating}
+			onChange={setRating}
+			itemStyles={{ itemShapes: Star, activeFillColor: 'var(--m-chat-primary-color)', inactiveFillColor: '#ffedd5' }}
+			style={{maxWidth: 100}}
+		/>
+	)
+};
+
+export default CustomRating;
